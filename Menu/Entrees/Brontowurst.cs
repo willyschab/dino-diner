36fb2823f12f4c3c9ca3242eadf9532c34225d50
Will Schabel <willschabel@ksu.edu>
2019-09-11 13:54:56 -0500

    Tests working

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class Brontowurst
+    {
+        private bool peppers = true;
+        private bool onions = true;
+        private bool wholeWheatBun = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (peppers) ingredients.Add("Peppers");
+                if (onions) ingredients.Add("Onions");
+                if (wholeWheatBun) ingredients.Add("Whole Wheat Bun");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 6.52;
+            this.Calories = 483;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onions = false;
+        }
+
+        public void HoldBun()
+        {
+            this.wholeWheatBun = false;
+        }
+    }
+}
---
+++ Menu/Entrees/TRexKingBurger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class TRexKingBurger
+    {
+        private bool peppers = true;
+        private bool onions = true;
+        private bool wholeWheatBun = true;
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (peppers) ingredients.Add("Peppers");
+                if (onions) ingredients.Add("Onions");
+                if (wholeWheatBun) ingredients.Add("Whole Wheat Bun");
+                return ingredients;
+            }
+        }
+
+        public TRexKingBurger()
+        {
+            this.Price = 4.25;
+            this.Calories = 59;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onions = false;
+        }
+
+        public void HoldBun()
+        {
+            this.wholeWheatBun = false;
+        }
+    }
+}

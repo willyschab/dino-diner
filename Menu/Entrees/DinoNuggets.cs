fb41c9765a087fda0fb00f00728a9d93fbcc8ea2
Will Schabel <willschabel@ksu.edu>
2019-09-12 10:17:22 -0500

    Not fixed

--- Menu/Entrees/DinoNuggets.cs
+++ Menu/Entrees/DinoNuggets.cs
@@ -4,20 +4,15 @@ namespace DinoDiner.Menu.Entrees
 {
     public class DinoNuggets
     {
-        private bool peppers = true;
-        private bool onions = true;
-        private bool wholeWheatBun = true;
         public double Price { get; set; }
         public uint Calories { get; set; }
+        public int  NuggetCount { get; set; }
 
         public List<string> Ingredients
         {
             get
             {
-                List<string> ingredients = new List<string>() { "Brautwurst" };
-                if (peppers) ingredients.Add("Peppers");
-                if (onions) ingredients.Add("Onions");
-                if (wholeWheatBun) ingredients.Add("Whole Wheat Bun");
+                List<string> ingredients = new List<string>() { "Chicken Nugget" };
                 return ingredients;
             }
         }
@@ -26,21 +21,12 @@ namespace DinoDiner.Menu.Entrees
         {
             this.Price = 4.25;
             this.Calories = 59;
+            this.NuggetCount = 6;
         }
 
-        public void HoldPeppers()
+        public void AddNugget()
         {
-            this.peppers = false;
-        }
-
-        public void HoldOnion()
-        {
-            this.onions = false;
-        }
-
-        public void HoldBun()
-        {
-            this.wholeWheatBun = false;
+            NuggetCount++;
         }
     }
 }
